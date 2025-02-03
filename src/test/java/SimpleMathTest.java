541a9c759b16657c80f3bdb94207f2f10060ab3d
FelipeArruda2002 <felipearruda.estudos@gmail.com>
2025-02-02 21:31:51 -0300

    Using life cycles

--- src/test/java/SimpleMathTest.java
+++ src/test/java/SimpleMathTest.java
@@ -1,16 +1,34 @@
 import com.felipearruda.math.SimpleMath;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class SimpleMathTest {
+
+    static SimpleMath simpleMath;
+
+    @BeforeAll
+    static void beforeAllMethod() {
+        System.out.println("Running before all method....");
+    }
+
+    @AfterAll
+    static void afterAllMethod() {
+        System.out.println("Running after all method....");
+    }
+
+    @BeforeEach
+    void setup() {
+        simpleMath = new SimpleMath();
+        System.out.println("Running before each method....");
+    }
+
+    @AfterEach
+    void afterEachMethod() {
+        System.out.println("Running after each method....");
+    }
 
     @Test
     @DisplayName("Sum Test(10 + 10) should return 20")
     public void testSum() {
-        SimpleMath simpleMath = new SimpleMath();
-
         Double actual = simpleMath.sum(10.0, 10.0);
 
         Assertions.assertEquals(20, actual, () -> "Failed test did not produce the expected result");
@@ -19,8 +37,6 @@ public class SimpleMathTest {
     @Test
     @DisplayName("Subtraction test (10 - 10) should return 0")
     public void testSubtraction() {
-        SimpleMath simpleMath = new SimpleMath();
-
         Double actual = simpleMath.subtraction(10.0, 10.0);
 
         Assertions.assertEquals(0, actual, () -> "Failed test did not produce the expected result");
@@ -29,8 +45,6 @@ public class SimpleMathTest {
     @Test
     @DisplayName("Multiplication test (10 x 5) should return 50")
     public void testMultiplication() {
-        SimpleMath simpleMath = new SimpleMath();
-
         Double actual = simpleMath.multiplication(10.0, 5.0);
 
         Assertions.assertEquals(50, actual, () -> "Failed test did not produce the expected result");
@@ -39,8 +53,6 @@ public class SimpleMathTest {
     @Test
     @DisplayName("Division test(10/2) should return 5")
     public void testDivision() {
-        SimpleMath simpleMath = new SimpleMath();
-
         Double actual = simpleMath.division(10.0, 2.0);
 
         Assertions.assertEquals(5, actual, () -> "Failed test did not produce the expected result");
@@ -49,8 +61,6 @@ public class SimpleMathTest {
     @Test
     @DisplayName("Mean test((10+2)/2) should return 6")
     public void testMean() {
-        SimpleMath simpleMath = new SimpleMath();
-
         Double actual = simpleMath.mean(10.0, 2.0);
 
         Assertions.assertEquals(6, actual, () -> "Failed test did not produce the expected result");
@@ -59,8 +69,6 @@ public class SimpleMathTest {
     @Test
     @DisplayName("Testing the square root of 9 should return 3")
     public void testSquareRoot() {
-        SimpleMath simpleMath = new SimpleMath();
-
         Double actual = simpleMath.squareRoot(9.0);
 
         Assertions.assertEquals(3, actual, () -> "Failed test did not produce the expected result");
