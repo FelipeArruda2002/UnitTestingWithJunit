fdf8f1d6c199bcb74dd495a4950d750f5be56457
FelipeArruda2002 <felipearruda.estudos@gmail.com>
2025-02-04 16:41:21 -0300

    Repeating test with @RepeatedTest

---
+++ src/test/java/SimpleMathRepeatedTest.java
@@ -0,0 +1,24 @@
+import com.felipearruda.math.SimpleMath;
+import org.junit.jupiter.api.*;
+
+public class SimpleMathRepeatedTest {
+
+    static SimpleMath simpleMath;
+
+    @BeforeEach
+    public void setup() {
+        simpleMath = new SimpleMath();
+    }
+
+    @RepeatedTest(value = 3, name = "{displayName}. Repetition "
+    + "{currentRepetition} of {totalRepetitions}!")
+    @DisplayName("Division by zero should return an Arithmetic Exception")
+    public void testDivisionByZero() {
+        String expectedMessage = "Impossible to division by zero!";
+
+        ArithmeticException arithmeticException = Assertions.assertThrows(ArithmeticException.class,
+                () -> simpleMath.division(10.0, 0.0), "Division by zero did not return an arithmetic exception");
+
+        Assertions.assertEquals(expectedMessage, arithmeticException.getMessage(), "Unexpected execution message");
+    }
+}
