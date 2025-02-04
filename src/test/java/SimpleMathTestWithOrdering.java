15bc1668d5b8fb10d8022ac011ef579147dd5a36
FelipeArruda2002 <felipearruda.estudos@gmail.com>
2025-02-04 17:23:57 -0300

    @TestMethodOrder Index ordering with @Order

--- src/test/java/SimpleMathTestWithOrdering.java
+++ src/test/java/SimpleMathTestWithOrdering.java
@@ -1,26 +1,31 @@
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.MethodName.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SimpleMathTestWithOrdering {
 
     @Test
+    @Order(4)
     void testC() {
         System.out.println("Running test C...");
     }
 
     @Test
+    @Order(1)
     void testB() {
         System.out.println("Running test B...");
     }
 
     @Test
+    @Order(3)
     void testD() {
         System.out.println("Running test D...");
     }
 
     @Test
+    @Order(2)
     void testA() {
         System.out.println("Running test A...");
     }
