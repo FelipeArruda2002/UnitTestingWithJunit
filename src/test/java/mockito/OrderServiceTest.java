40eb47191ef421bf2b3ad77ebddac6c9e7a1b5c7
FelipeArruda2002 <felipearruda.estudos@gmail.com>
2025-03-06 11:51:38 -0300

    Mocking a static method(LocalDateTime::now)

--- src/test/java/mockito/OrderServiceTest.java
+++ src/test/java/mockito/OrderServiceTest.java
@@ -8,12 +8,14 @@ import org.mockito.Mockito;
 import service.mockito.Order;
 import service.mockito.OrderService;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class OrderServiceTest {
 
     private  OrderService orderService;
     private final UUID defaultUuid = UUID.fromString("8d8b30e3-de52-4f1c-a71c-9905a8043dac");
+    private final LocalDateTime defaultLocalDateTime = LocalDateTime.of(2025, 3, 6, 11, 47);
 
     @BeforeEach
     void createOrderService() {
@@ -28,6 +30,17 @@ public class OrderServiceTest {
             Order result = orderService.createOrder("MacBook Pro", 2L, null);
 
             Assertions.assertEquals(defaultUuid.toString(), result.getId());
+        }
+    }
+
+    @Test
+    void shouldIncludeCurrentTimeWhenCreatingANewOrder() {
+        try (MockedStatic<LocalDateTime> localDateTimeMocked = Mockito.mockStatic(LocalDateTime.class)){
+            localDateTimeMocked.when(LocalDateTime::now).thenReturn(defaultLocalDateTime);
+
+            Order result = orderService.createOrder("Iphone 13", 1L, "7");
+
+            Assertions.assertEquals(defaultLocalDateTime, result.getCreationDate());
         }
     }
 
